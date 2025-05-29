0fedf1eaf983585575f84d6cddda08b951c3a4cb
Marcy <marcelly.nascimento84@gmail.com>
2025-05-29 13:20:02 -0300

    feat: finalização do layout inicial das telas de good ending e game over

--- Assets/Scripts/MusicManager.cs
+++ Assets/Scripts/MusicManager.cs
@@ -5,6 +5,7 @@ public class MusicManager : MonoBehaviour
     [SerializeField] private AudioSource musicSource;
     [SerializeField] private AudioClip gameMusic;
     [SerializeField] private AudioClip gameOverMusic;
+    [SerializeField] private AudioClip goodEndingMusic;
 
     public void PlayGameMusic()
     {
@@ -20,6 +21,14 @@ public class MusicManager : MonoBehaviour
         if (musicSource.clip != gameOverMusic)
         {
             musicSource.clip = gameOverMusic;
+            musicSource.Play();
+        }
+    }
+    public void PlayGoodEndingMusic()
+    {
+        if (musicSource.clip != goodEndingMusic)
+        {
+            musicSource.clip = goodEndingMusic;
             musicSource.Play();
         }
     }
